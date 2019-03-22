641905386e0ddd6184bc2e03b84dbbd06a4253d7
xypnox <xypnox@gmail.com>
2019-03-22 06:46:40 +0530

    Redesigned the social section

---
+++ main.js
@@ -0,0 +1,7 @@
+AOS.init();
+
+function changeText(parentClass, text) {
+    var display = document.getElementsByClassName(parentClass)[0]
+    display = display.getElementsByClassName("inner-text")[0];
+    display.textContent = text;
+}
