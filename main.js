1ed9c6c39c1d6f0d1a244154867555470faf056a
xypnox <xypnox@gmail.com>
2019-06-12 18:13:10 +0530

    Dark mode baby :dancer: :fire: :black_circle:

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
 AOS.init();
-
-function changeText(parentClass, text) {
-  var display = document.getElementsByClassName(parentClass)[0];
-  display = display.getElementsByClassName('inner-text')[0];
-  display.textContent = text;
-}
