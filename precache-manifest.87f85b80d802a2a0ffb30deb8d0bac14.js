30d0fbdf65633f846bd0963e13e2f0124c8647e1
xypnox <xypnox@gmail.com>
2020-12-22 17:05:01 +0530

    Deploy react version

---
+++ precache-manifest.87f85b80d802a2a0ffb30deb8d0bac14.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ac775d4a629fbf608f1b3b1e7b8628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066fb7faca7efd47f44a",
+    "url": "/static/css/main.2997b2f5.chunk.css"
+  },
+  {
+    "revision": "b6d3acecea1210426e25",
+    "url": "/static/js/2.54523976.chunk.js"
+  },
+  {
+    "revision": "d6c216e02fc1ef7a85d28c1c3d094415",
+    "url": "/static/js/2.54523976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066fb7faca7efd47f44a",
+    "url": "/static/js/main.80e39fa2.chunk.js"
+  },
+  {
+    "revision": "13c597a67074611b70b8",
+    "url": "/static/js/runtime-main.0fdeae1d.js"
+  },
+  {
+    "revision": "a7f4d19938f951fb0f4f3cfac97133e4",
+    "url": "/static/media/backend.a7f4d199.svg"
+  },
+  {
+    "revision": "e788eed0cdcc2fa387296589f0d857cb",
+    "url": "/static/media/blag.e788eed0.png"
+  },
+  {
+    "revision": "46723c75258dcf9773e6fbc7039ffa07",
+    "url": "/static/media/code.46723c75.svg"
+  },
+  {
+    "revision": "f69cdc07607fe047e002afdf57df31fa",
+    "url": "/static/media/design.f69cdc07.svg"
+  },
+  {
+    "revision": "a13169507f31830a9323cf4e3836b27f",
+    "url": "/static/media/dotfiles.a1316950.png"
+  },
+  {
+    "revision": "00f3a2a1af11dd754ccc22f7bbace4ed",
+    "url": "/static/media/fifthtry.00f3a2a1.svg"
+  },
+  {
+    "revision": "d5fcf31563ac8feadb711368425f086f",
+    "url": "/static/media/finbox-logo.d5fcf315.svg"
+  },
+  {
+    "revision": "7a3d334b3be050a20205d82273919135",
+    "url": "/static/media/fontstaller.7a3d334b.png"
+  },
+  {
+    "revision": "ffd3f1c8d7f9e1024cae332c4da80117",
+    "url": "/static/media/frontend.ffd3f1c8.svg"
+  },
+  {
+    "revision": "09bdb6a37a619f49b29696dcf2c968c4",
+    "url": "/static/media/gyft2.09bdb6a3.png"
+  },
+  {
+    "revision": "2df2cc8b0efd4bfbeb7195d16a7547b1",
+    "url": "/static/media/gymkhana.2df2cc8b.png"
+  },
+  {
+    "revision": "d02bcf21e490bd561060b110154bbc11",
+    "url": "/static/media/kalrav.d02bcf21.png"
+  },
+  {
+    "revision": "a8991552399be6fe6e07db414728694f",
+    "url": "/static/media/koss.a8991552.png"
+  },
+  {
+    "revision": "132e7639d4c677ce5645d8fc73a02500",
+    "url": "/static/media/kwoc.132e7639.png"
+  },
+  {
+    "revision": "c239e495af6c18abfbdc7c1a6dd30819",
+    "url": "/static/media/logo.c239e495.svg"
+  },
+  {
+    "revision": "d1b1f35ec66b6ed18637e041955feee4",
+    "url": "/static/media/lttkgp.d1b1f35e.svg"
+  },
+  {
+    "revision": "c2ade9691aec07ca5da08a83ae9f2f0b",
+    "url": "/static/media/marg.c2ade969.png"
+  },
+  {
+    "revision": "090d667fee0d78d8156c0f98e891b4f7",
+    "url": "/static/media/metakgp.090d667f.png"
+  },
+  {
+    "revision": "de26e3f1d0f6316a80c6b8abeb42b0a8",
+    "url": "/static/media/poems.de26e3f1.png"
+  },
+  {
+    "revision": "079218a79432960a65f68876871c6630",
+    "url": "/static/media/posters.079218a7.png"
+  },
+  {
+    "revision": "f5cc82c71c7c59bc3e7f547ba91eeb28",
+    "url": "/static/media/superpowers.f5cc82c7.svg"
+  },
+  {
+    "revision": "3aedb2cfbff01fcb5c34252309b10ba4",
+    "url": "/static/media/todxpy.3aedb2cf.png"
+  },
+  {
+    "revision": "eb223338f0ebc5011e8655c0e03c1a6b",
+    "url": "/static/media/ui-ux.eb223338.svg"
+  },
+  {
+    "revision": "8be412c4c4f9876556665907b40e3b5b",
+    "url": "/static/media/work-art.8be412c4.svg"
+  },
+  {
+    "revision": "2ce6b2bf817aeac8064ae37cefdfdfde",
+    "url": "/static/media/xydark.2ce6b2bf.png"
+  }
+]);
