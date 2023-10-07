f29eebec8ea97d8ff835b1580d9acc46d63c6e7c
xypnox <xypnox@gmail.com>
2023-10-07 20:22:55 +0530

    Add covers page with covers and their thumbnails

---
+++ src/content/coverData.ts
@@ -0,0 +1,174 @@
+export interface CoverCollection {
+  name: string;
+  description?: string;
+  covers: string[];
+}
+
+export const Covers: CoverCollection[] = [
+  {
+    name: "Chronological",
+    description: "Playlists that are created in chronological order, the tracks are added in the month they were listened to most or discovered in",
+    covers: [
+      "2021.01.webp",
+      "2021.02.webp",
+      "2021.03.webp",
+      "2021.04.01.webp",
+      "2021.04.02.webp",
+      "2021.04.webp",
+      "2021.05.01.webp",
+      "2021.05.02.webp",
+      "2021.05.webp",
+      "2021.06.01.webp",
+      "2021.06.ii.webp",
+      "2021.06.webp",
+      "2021.07.ii.webp",
+      "2021.07.i.webp",
+      "2021.08.ii.webp",
+      "2021.08.i.webp",
+      "2021.09.ii.webp",
+      "2021.09.i.webp",
+      "2021.10.ii.webp",
+      "2021.10.i.webp",
+      "2021.11.ii.webp",
+      "2021.11.i.webp",
+      "2021.12.ii.webp",
+      "2021.12.i.webp",
+      "2022.01.ii.webp",
+      "2022.01.i.webp",
+      "2022.02.ii.webp",
+      "2022.02.i.webp",
+      "2022.03.ii.webp",
+      "2022.03.i.webp",
+      "2022.04.ii.webp",
+      "2022.04.i.webp",
+      "2022.05.ii.webp",
+      "2022.05.i.webp",
+      "2022.06.ii.webp",
+      "2022.06.i.webp",
+      "2022.07.ii.webp",
+      "2022.07.i.webp",
+      "2022.08.ii.webp",
+      "2022.08.i.webp",
+      "2022.09.ii.webp",
+      "2022.09.i.webp",
+      "2022.10.ii.webp",
+      "2022.10.i.webp",
+      "2022.11.ii.webp",
+      "2022.11.i.webp",
+      "2022.12.ii.webp",
+      "2022.12.i.webp",
+      "2022.webp",
+      "2023.01.ii.webp",
+      "2023.01.i.webp",
+      "2023.02.ii.webp",
+      "2023.02.i.webp",
+      "2023.03.ii.webp",
+      "2023.03.i.webp",
+      "2023.04.ii.webp",
+      "2023.04.i.webp",
+      "2023.05.ii.webp",
+      "2023.05.i.webp",
+      "2023.06.ii.webp",
+      "2023.06.i.webp",
+      "2023.07.ii.webp",
+      "2023.07.i.webp",
+      "2023.08.ii.webp",
+      "2023.08.i.webp",
+      "2023.09.ii.webp",
+      "2023.09.i.webp",
+    ]
+  },
+  {
+    name: "Corona",
+    description: "Playlists that were created during the COVID-19 pandemic, these are chronological as well",
+    covers: [
+      "CoronaX3.webp",
+      "CoronaX4.webp",
+      "CoronaX5.webp",
+      "CoronaX6.webp",
+      "CoronaX7.webp",
+      "CoronaX8.webp",
+      "CoronaX9.webp",
+      "CoronaX10.webp",
+      "CoronaX11.webp",
+      "CoronaX12.webp",
+      "CoronaX13.webp",
+      "Coronax14.webp",
+      "CoronaX15.webp",
+      "CoronaX16.webp",
+      "CoronaX17.webp",
+      "CoronaX18.webp",
+      "CoronaX19.webp",
+    ]
+  },
+  {
+    name: "Favourites",
+    description: "Playlists that store my favourites",
+    covers: [
+      "Favspot.webp",
+      "Absolute Feels.webp",
+      "Stuck in the head.webp",
+      "Rep.webp",
+    ]
+  },
+  {
+    name: "Musical",
+    description: "Playlists that for me carry an essence of music",
+    covers: [
+      "Eolian's.webp",
+      "Kvothe's.webp",
+      "ElvenHigh.webp",
+      "Music to my ears.webp",
+      "Beats.webp",
+      "Benchmarks.webp",
+    ]
+  },
+  {
+    name: "Louda",
+    description: "Playlists that focus on the loud and pumpy music, not Niki Lauda",
+    covers: [
+      "Haut.webp",
+      "hyperchill.webp",
+      "EDM.webp",
+      "chill.webp",
+      "pump.webp",
+    ]
+  },
+  {
+    name: "Occasions",
+    description: "Playlists that were created for a particular occasion",
+    covers: [
+      "Drifting in a nismo in tokyo.webp",
+      "driving-at-night.webp",
+      "Nightsky.webp",
+      "Journ.webp",
+      "mellow.webp",
+      "that warm fuzzy time.webp",
+      "Rooftopsun.webp",
+      "MyFi.webp",
+    ]
+  },
+  {
+    name: "Genres",
+    description: "Playlists that focus on a particular genre",
+    covers: [
+      // Genres
+      "Ghazals.webp",
+      "bollywood.webp",
+      "indian-rap.webp",
+      "Classical Melodies.webp",
+      "Farmhouse.webp",
+    ]
+  },
+  {
+    name: "Misc",
+    covers: [
+      // Misc
+      "collected.webp",
+      "fantasy-book-bg.webp",
+      "Broken.webp",
+    ]
+  },
+
+
+]
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,15 @@
 export const themeVars = {
   font: {
     family: 'Jost, sans-serif',
+    size: {
+      sm: "clamp(0.8rem, 0.21vw + 0.75rem, 0.94rem)",
+      base: "clamp(1rem, 0.38vw + 0.9rem, 1.25rem)",
+      md: "clamp(1.25rem, 0.64vw + 1.09rem, 1.67rem)",
+      lg: "clamp(1.56rem, 1.01vw + 1.31rem, 2.22rem)",
+      xl: "clamp(1.95rem, 1.55vw + 1.57rem, 2.96rem)",
+      xxl: "clamp(2.44rem, 2.32vw + 1.86rem, 3.95rem)",
+      xxxl: "clamp(3.05rem, 3.4vw + 2.2rem, 5.26rem)",
+    }
   },
   primary: {
     color: '#ff5370',
