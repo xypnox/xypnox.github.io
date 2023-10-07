640397ece0c0cb540bb51a7d772bab07d847e9f5
xypnox <xypnox@gmail.com>
2023-10-07 22:45:01 +0530

    Add icons

---
+++ src/content/contentTypes.ts
@@ -0,0 +1,5 @@
+export interface IconCollection {
+  name: string;
+  description?: string;
+  icons: string[];
+}
---
+++ src/content/iconsData.ts
@@ -0,0 +1,73 @@
+import type { IconCollection } from "./contentTypes";
+
+export const iconsData: IconCollection[] = [
+  {
+    name: "Companies",
+    description: "Logos of companies I've designed or tweaked",
+    icons: [
+      "Workduck.svg",
+      "Calry.svg",
+      "Nirah.svg",
+      "Fifthtry.svg",
+      "finbox.svg",
+    ]
+  },
+
+  {
+    name: "Projects",
+    description: "Logos of projects I've designed or tweaked",
+    icons: [
+      "lttkp.svg",
+      "ProjectMojo.svg",
+      "unscrupulous.svg",
+    ]
+  },
+
+  {
+    name: "KOSS",
+    description: "Logos of KOSS projects I've designed or tweaked",
+    icons: [
+      "kossgrad.svg",
+      "kossline.svg",
+      "kossnew.svg",
+      "kwoc.svg",
+      "gatherkoss.svg",
+    ]
+  },
+
+  {
+    name: "Self",
+    description: "Logos of my personal projects",
+    icons: [
+      "Blag.svg",
+      "Studio.svg",
+      "StudioGrad.svg",
+      "Notes.svg",
+      "Feather.svg",
+      "OldX.svg",
+    ]
+  },
+
+
+  {
+    name: "Miscellaneous",
+    description: "Logos of miscellaneous things",
+    icons: [
+      "birdfly.svg",
+      "birdhere.svg",
+      "Calc.svg",
+      "Coloria.svg",
+      "DripShot.svg",
+      "Hunt.svg",
+      "Keep.svg",
+      "Lock.svg",
+      "Lykos.svg",
+      "rocketship.svg",
+      "Tasks.svg",
+      "transfer.svg",
+      "transfer-color.svg",
+      "venom-spider.svg",
+      "Wiki.svg",
+    ]
+  }
+]
