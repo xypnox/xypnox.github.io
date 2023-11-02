d66569e8b15b248fc12d3a3b8c4da23f0f81adca
xypnox <xypnox@gmail.com>
2023-11-02 22:11:50 +0530

    Add blog posts to rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
 import rss from "@astrojs/rss"
-// import { getCollection } from 'astro:content';
+import { getCollection } from 'astro:content';
 
-export function GET(context) {
+export async function GET(context) {
+  const blog = await getCollection('blog');
   return rss({
     title: "Blag",
     description: "To muse over the words unmusable.",
     site: context.site,
-    items: [],
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      image: post.data.coverImage,
+      link: `/blag/${post.slug}`,
+    })),
   })
 }
