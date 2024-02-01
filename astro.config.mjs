237022c86309778707765cd3659c354c8de1a618
xypnox <xypnox@gmail.com>
2024-02-01 20:58:38 +0530

    Add draft post, and test mermaid highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import remarkBreaks from 'remark-breaks';
+import rehypeMermaid from 'rehype-mermaid';
+import rehypeShiki from '@shikijs/rehype'
 import mdx from "@astrojs/mdx";
 
 import shikiTheme from "./src/styles/xydark.json";
@@ -12,9 +14,12 @@ export default defineConfig({
   integrations: [solidJs(), mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkBreaks],
-    shikiConfig: {
-      theme: shikiTheme,
-    }
+    rehypePlugins: [
+      rehypeMermaid,
+      [rehypeShiki, {
+        theme: shikiTheme,
+      }] ],
+    syntaxHighlight: false,
   },
   build: {
     site: 'https://www.xypnox.com',
