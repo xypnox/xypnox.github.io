0f2a9a0ec26dc0460290dc6520106167620e11eb
xypnox <xypnox@gmail.com>
2024-03-14 22:04:58 +0530

    fixtest: only log for fetch images

--- src/buildScripts/fetchImages.ts
+++ src/buildScripts/fetchImages.ts
@@ -1,53 +1,63 @@
-import { ListObjectsCommand, GetObjectCommand } from '@aws-sdk/client-s3';
-import { s3Client, env, IMAGE_DIRECTORY } from '../lib/s3'
-import { promises as fs, createWriteStream } from 'fs';
-import path from 'path';
+// import { ListObjectsCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+// import { env, IMAGE_DIRECTORY } from '../lib/s3'
+// import { promises as fs, createWriteStream } from 'fs';
+// import path from 'path';
 
 const fetchImagesToDirectory = async (directory: string, bucketName: string) => {
-  console.log('Fetching images to directory:', directory, bucketName);
+  const env = {
+    url: process.env.IMAGE_API_URL,
+    region: process.env.IMAGE_API_REGION,
+    bucket: process.env.IMAGE_API_BUCKET,
+    // cdn: process.env.IMAGE_CDN_URL
+  };
+  console.log('Fetching images to directory:', directory, bucketName, {
+    env
+  });
 
-  try {
-    const { Contents } = await s3Client.send(new ListObjectsCommand({
-      Bucket: bucketName
-    }));
+  return;
 
-    if (!Contents) {
-      console.log('No contents found');
-      return;
-    }
+  // try {
+  //   const { Contents } = await s3Client.send(new ListObjectsCommand({
+  //     Bucket: bucketName
+  //   }));
 
-    await fs.mkdir(directory, { recursive: true });
+  //   if (!Contents) {
+  //     console.log('No contents found');
+  //     return;
+  //   }
 
-    for (const content of Contents) {
-      if (!content.Key) continue;
+  //   await fs.mkdir(directory, { recursive: true });
 
-      const objectPath = path.join(directory, content.Key);
-      const dir = path.dirname(objectPath);
+  //   for (const content of Contents) {
+  //     if (!content.Key) continue;
 
-      // Ensure the directory structure exists
-      await fs.mkdir(dir, { recursive: true });
+  //     const objectPath = path.join(directory, content.Key);
+  //     const dir = path.dirname(objectPath);
 
-      const objectData = await s3Client.send(new GetObjectCommand({
-        Bucket: bucketName,
-        Key: content.Key,
-      }));
+  //     // Ensure the directory structure exists
+  //     await fs.mkdir(dir, { recursive: true });
 
-      const fileStream = createWriteStream(objectPath);
-      if (!objectData.Body) continue;
-      objectData.Body.transformToByteArray().then((data) => {
-        fileStream.write(data);
-      });
+  //     const objectData = await s3Client.send(new GetObjectCommand({
+  //       Bucket: bucketName,
+  //       Key: content.Key,
+  //     }));
 
-      console.log(`File downloaded: ${objectPath}`);
-    }
-  } catch (error) {
-    console.error('Error fetching images:', error);
-  }
+  //     const fileStream = createWriteStream(objectPath);
+  //     if (!objectData.Body) continue;
+  //     objectData.Body.transformToByteArray().then((data) => {
+  //       fileStream.write(data);
+  //     });
+
+  //     console.log(`File downloaded: ${objectPath}`);
+  //   }
+  // } catch (error) {
+  //   console.error('Error fetching images:', error);
+  // }
 };
 
 
 (async () => {
-  fetchImagesToDirectory(IMAGE_DIRECTORY, env.bucket!).then(() => {
+  fetchImagesToDirectory('', '').then(() => {
     console.log('Images fetched')
   }).catch((error) => {
     console.error('Error:', error)
