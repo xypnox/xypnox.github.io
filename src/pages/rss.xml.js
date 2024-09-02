f0134cd764e657e4693de379d49afa1085fb6753
xypnox <xypnox@gmail.com>
2024-09-02 20:25:13 +0530

    Fix rss feed url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,8 @@
-import rss from "@astrojs/rss"
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const blog = await getCollection('blog');
+  const blog = await getCollection("blog");
   return rss({
     title: "Blag",
     description: "To muse over the words unmusable.",
@@ -14,7 +14,7 @@ export async function GET(context) {
         pubDate: post.data.date,
         description: post.data.description,
         image: post.data.coverImage,
-        link: `/blag/${post.slug}`,
+        link: `/blag/posts/${post.slug}`,
       })),
-  })
+  });
 }
